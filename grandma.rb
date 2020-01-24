906855ae946f2703fb97da50d66f505a975c42a4
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-24 06:40:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  else phrase. upcase
+  else phrase == "DID YOU FEED THE CAT TODAY?!"
     "NO, NOT SINCE 1938!"
    end
  end 
